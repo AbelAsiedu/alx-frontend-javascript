361d96c7a8246d0d462980b11a27ce137c1cd133
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-07-24 22:06:16 +0000

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,10 @@
-// 0-promise.js
-
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulating an asynchronous operation (like fetching data)
-        setTimeout(() => {
-            resolve("Response from API"); // You can customize the response
-        }, 1000); // Simulate a delay of 1 second
-    });
+export default function getResponseFromAPI() {
+  const success = true;
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
 }
-
-export default getResponseFromAPI;
