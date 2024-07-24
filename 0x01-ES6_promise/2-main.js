2823c0cff6c807f3968d61330b163df7b94d7c1d
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-07-24 22:22:46 +0000

    Update 2-main.js

--- 0x01-ES6_promise/2-main.js
+++ 0x01-ES6_promise/2-main.js
@@ -1,6 +1,6 @@
-// 2-main.js
-import handleResponseFromAPI from "./2-then.js";
-
-const promise = Promise.resolve();
-handleResponseFromAPI(promise);
-
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}
