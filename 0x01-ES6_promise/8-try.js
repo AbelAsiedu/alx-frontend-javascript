2acf2cf279a21d00833b6019c0807178f12b32ff
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-07-24 22:40:08 +0000

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,7 @@
-// 8-try.js
 export default function divideFunction(numerator, denominator) {
-    if (denominator === 0) {
-        throw new Error('cannot divide by 0');
-    }
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else {
     return numerator / denominator;
+  }
 }
-
