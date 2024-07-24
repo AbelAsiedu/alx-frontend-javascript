07831630cf2ae00afcce19da090f22f4f380fcbb
AbelAsiedu <141195146+AbelAsiedu@users.noreply.github.com>
2024-07-24 22:16:05 +0000

    Update 1-main.js

--- 0x01-ES6_promise/1-main.js
+++ 0x01-ES6_promise/1-main.js
@@ -1,6 +1,4 @@
-// 1-main.js
-import getFullResponseFromAPI from './1-promise.js';
+import getFullResponseFromAPI from './1-promise';
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
-
